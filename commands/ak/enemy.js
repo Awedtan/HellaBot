41512b1f5f01758059c6bdcf9319ece970c7abb1
Awedtan <31903483+Awedtan@users.noreply.github.com>
2023-05-06 20:32:26 -0400

    Fix undefined enemy stats

--- commands/ak/enemy.js
+++ commands/ak/enemy.js
@@ -16,9 +16,22 @@ module.exports = {
 
         if (enemyName in enemyDict) {
             const enemyInfo = enemyDict[enemyName].enemy;
-            const enemyStats = enemyDict[enemyName].enemyData.Value[0].enemyData.attributes;
+            const enemyData = enemyDict[enemyName].enemyData.Value[0].enemyData;
             const icon = new AttachmentBuilder(iconPath);
             const image = new AttachmentBuilder(`./${enemyImagePath}/${enemyInfo.enemyId}.png`);
+
+            const hp = enemyData.attributes.maxHp.m_value.toString();
+            const atk = enemyData.attributes.atk.m_value.toString();
+            const def = enemyData.attributes.def.m_value.toString();
+            const res = enemyData.attributes.magicResistance.m_value.toString();
+            const weight = enemyData.attributes.massLevel.m_value.toString();
+            const life = enemyDict[enemyName].enemyData.Value[0].enemyData.lifePointReduce.m_defined ? enemyDict[enemyName].enemyData.Value[0].enemyData.lifePointReduce.m_value.toString() : '1';
+
+            const silenceImmune = enemyData.attributes.silenceImmune.m_defined ? enemyData.attributes.silenceImmune.m_value : false;
+            const stunImmune = enemyData.attributes.stunImmune.m_defined ? enemyData.attributes.stunImmune.m_value : false;
+            const sleepImmune = enemyData.attributes.sleepImmune.m_defined ? enemyData.attributes.sleepImmune.m_value : false;
+            const frozenImmune = enemyData.attributes.frozenImmune.m_defined ? enemyData.attributes.frozenImmune.m_value : false;
+            const levitateImmune = enemyData.attributes.levitateImmune.m_defined ? enemyData.attributes.levitateImmune.m_value : false;
 
             const embed = new EmbedBuilder()
                 .setColor(0x0099FF)
@@ -27,17 +40,17 @@ module.exports = {
                 .setDescription(enemyInfo.description)
                 .setThumbnail(`attachment://${enemyInfo.enemyId}.png`)
                 .addFields(
-                    { name: '❤️ HP', value: enemyStats.maxHp.m_value.toString(), inline: true },
-                    { name: '⚔️ ATK', value: enemyStats.atk.m_value.toString(), inline: true },
-                    { name: '🛡️ DEF', value: enemyStats.def.m_value.toString(), inline: true },
-                    { name: '✨ RES', value: enemyStats.magicResistance.m_value.toString(), inline: true },
-                    { name: '⚖️ Weight', value: enemyStats.massLevel.m_value.toString(), inline: true },
-                    { name: '💔 Life Points', value: enemyDict[enemyName].enemyData.Value[0].enemyData.lifePointReduce.m_value.toString(), inline: true },
-                    { name: 'Silence', value: enemyStats.silenceImmune.m_value ? '❌' : '🤐', inline: true },
-                    { name: 'Stun', value: enemyStats.stunImmune.m_value ? '❌' : '😵', inline: true },
-                    { name: 'Sleep', value: enemyStats.sleepImmune.m_value ? '❌' : '😴', inline: true },
-                    { name: 'Freeze', value: enemyStats.frozenImmune.m_value ? '❌' : '🥶', inline: true },
-                    { name: 'Levitate', value: enemyStats.levitateImmune.m_value ? '❌' : '🌪️', inline: true });
+                    { name: '❤️ HP', value: hp, inline: true },
+                    { name: '⚔️ ATK', value: atk, inline: true },
+                    { name: '🛡️ DEF', value: def, inline: true },
+                    { name: '✨ RES', value: res, inline: true },
+                    { name: '⚖️ Weight', value: weight, inline: true },
+                    { name: '💔 Life Points', value: life, inline: true },
+                    { name: 'Silence', value: silenceImmune ? '❌' : '🤐', inline: true },
+                    { name: 'Stun', value: stunImmune ? '❌' : '😵', inline: true },
+                    { name: 'Sleep', value: sleepImmune ? '❌' : '😴', inline: true },
+                    { name: 'Freeze', value: frozenImmune ? '❌' : '🥶', inline: true },
+                    { name: 'Levitate', value: levitateImmune ? '❌' : '🌪️', inline: true });
 
             await interaction.reply({ embeds: [embed], files: [icon, image] });
         }
