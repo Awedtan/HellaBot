64c86b78f7b413ecf22fb0f61a16752fbf127a34
Awedtan <31903483+Awedtan@users.noreply.github.com>
2024-09-11 21:58:01 -0400

    add ping command fr

---
+++ src/commands/ping.ts
@@ -0,0 +1,61 @@
+import { ButtonInteraction, ChatInputCommandInteraction, EmbedBuilder, Interaction, SlashCommandBuilder } from 'discord.js';
+import Command from '../structures/Command';
+import { buildPingMessage } from '../utils/build';
+
+export default class PingCommand implements Command {
+    data = new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Get bot network latency') as SlashCommandBuilder;
+    name = 'Ping';
+    description = ['Get bot network latency.'];
+    usage = [
+        '`/ping`'
+    ];
+    async ping(interaction: ChatInputCommandInteraction | ButtonInteraction) {
+        const pingEmbed = await buildPingMessage();
+        await interaction.editReply(pingEmbed);
+
+        const embed = EmbedBuilder.from(pingEmbed.embeds[0]);
+        const a = Date.now();
+        fetch('https://discord.com/api/v10').then(res => {
+            const apiField = embed.data.fields.find(field => field.name === 'HellaAPI');
+            const githubField = embed.data.fields.find(field => field.name === 'GitHub');
+            embed.setFields([
+                { name: 'Discord', value: res.ok ? `${Date.now() - a}ms` : 'Unreachable', inline: true },
+                githubField,
+                apiField
+            ]);
+            interaction.editReply({ embeds: [embed] });
+        });
+        fetch('https://api.github.com/').then(res => {
+            const discordField = embed.data.fields.find(field => field.name === 'Discord');
+            const apiField = embed.data.fields.find(field => field.name === 'HellaAPI');
+            embed.setFields([
+                discordField,
+                { name: 'GitHub', value: res.ok ? `${Date.now() - a}ms` : 'Unreachable', inline: true },
+                apiField
+            ]);
+            interaction.editReply({ embeds: [embed] });
+        });
+        fetch('https://awedtan.ca/api/about').then(async res => {
+            const discordField = embed.data.fields.find(field => field.name === 'Discord');
+            const githubField = embed.data.fields.find(field => field.name === 'GitHub');
+            embed.setFields([
+                discordField,
+                githubField,
+                { name: 'HellaAPI', value: (await res.json()).length ? `${Date.now() - a}ms` : 'Unreachable', inline: true }
+            ]);
+            interaction.editReply({ embeds: [embed] });
+        });
+    }
+    async execute(interaction: ChatInputCommandInteraction) {
+        await interaction.deferReply();
+        return await this.ping(interaction);
+    }
+    async buttonResponse(interaction: ButtonInteraction, idArr: string[]) {
+        if (idArr[1] === 'refresh') {
+            await interaction.deferUpdate();
+            return await this.ping(interaction);
+        }
+    }
+}
